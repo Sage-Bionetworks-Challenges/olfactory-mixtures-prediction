cdc8ad7e68f46b75a9087968eeb9e4ea4faaaf5e
mdsage1 <maria.diaz@sagebase.org>
2025-04-17 09:33:53 -0700

    conflict resolution

--- validate.py
+++ validate.py
@@ -73,7 +73,7 @@ def check_unknown_mixtures(gold, pred):
 def check_nan_values(pred):
     """Check for NAN predictions."""
     # Check if all values are NaN.
-    if pred.isna().all().all() is True:
+    if pred.isna().all().all():
         return "All columns contain NaN values."
 
     return ""
